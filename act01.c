8d85abf53f5f1f4fe4f9098f6f3dd1f65a2860c7
caffs <aburaksahin@hotmail.com.tr>
2022-02-03 15:17:25 +0300

    Its my firs git file

---
+++ act01.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /* w3resource basic declarations and espressions 001
+    printf("Name     : Burak Sahin\n");
+    printf("Birthday : 01.01.01\n");
+    printf("Mobile   : 0555 555 55 55\n");
+    */
+
+    /* w3resource basic declarations and espressions 002
+    printf("We are using C18!");
+    */
+
+    /* w3resource basic declarations and espressions 003
+
+    printf("######\n");
+    printf("#\n");
+    printf("#\n");
+    printf("####\n");
+    printf("#\n");
+    printf("#\n");
+    printf("    ######\n");
+    printf("  ##       ##\n");
+    printf("#\n");
+    printf("#\n");
+    printf("#\n");
+    printf("#\n");
+    printf("#\n");
+    printf("#\n");
+    printf("  ##       ##\n");
+    printf("    ######\n");
+
+    */
+    /* w3resource basic declarations and espressions 004
+
+    char x='X';
+    char m='M';
+    char l='L';
+
+    printf("The reverse of %c%c%c is %c%c%c",x,m,l,l,m,x);
+    */
+    /* w3resource basic declarations and espressions 005
+    int height=7;
+    int witdh=5;
+    printf("Perimeter of the rectangle is %d inches \n", (height+witdh)*2);
+    printf("Area of the rectangle is %d inches", height*witdh);
+    */
+    /* w3resource basic declarations and espressions 006
+    const float pinum=3.14;
+    float rad;
+    printf("give rad value ");
+    scanf("%f",&rad);
+    printf("perimeter: %f\n",2* pinum *rad);
+    printf("area : %f",pinum*rad*rad);
+    */
+
+}
