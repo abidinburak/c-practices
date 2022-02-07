ef2d83d95723056089d8cf0a19114b747fa12c7c
caffs <aburaksahin@hotmail.com.tr>
2022-02-07 23:19:54 +0300

    c programming beginner examples 06

---
+++ act06.c
@@ -0,0 +1,79 @@
+#include<stdio.h>
+
+int main()
+{
+
+    /*  w3resource basic declarations and expressions 23
+    float first, second, third, perimeter;
+    printf("input first ");
+    scanf("%f",&first);
+    printf("input second ");
+    scanf("%f",&second);
+    printf("input third ");
+    scanf("%f",&third);
+    if(first < (second+third) && second < (first+third) && third < (second+first))
+    {
+    perimeter=first+second+third;
+    printf("%1.f",perimeter);
+    } */
+    
+    
+    /*w3resource basic declarations and expressions 24
+    int first, second;
+    printf("input first ");
+    scanf("%d",&first);
+    printf("input second ");
+    scanf("%d",&second);
+    if(first%second==0 || second%first==0)
+    printf("multiplied");
+    else
+    printf("not");
+    */
+    
+    
+    /*w3resource basic declarations and expressions 25
+    int months;
+    printf("input months: ");
+    scanf("%d",&months);
+    switch (months) {
+    case 1:
+    printf("jan");
+    break;
+    case 2:
+    printf("feb");
+    break;
+    case 3:
+    printf("march");
+    break;
+    case 4:
+    printf("april");
+    break;
+    case 5:
+    printf("may");
+    break;
+    case 6:
+    printf("jule");
+    break;
+    case 7:
+    printf("july");
+    break;
+    case 8:
+    printf("aug");
+    break;
+    case 9:
+    printf("oct");
+    break;
+    case 10:
+    printf("sep");
+    break;
+    case 11:
+    printf("nov");
+    break;
+    case 12:
+    printf("dec");
+    break;
+    }
+    */
+    
+    return 0;
+}
